a932b24b2e98e2e5e1c9c4ce3df4b5cdb54a0662
= <victorh.cepil@hotmail.com>
2020-08-31 17:06:17 -0300

    chore: Resolution

--- src/app.js
+++ src/app.js
@@ -1,33 +1,89 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const repository = repositories.find((repo) => repo.id === id);
+  const { url, title, techs } = request.body;
+
+  if (!repository) {
+    return response.status(400).json("Repository not found");
+  }
+
+  Object.assign(repository, {
+    url,
+    title,
+    techs,
+  });
+
+  const newArray = repositories.filter((repo) => repo.id !== id);
+  newArray.push(repository);
+
+  repositories = newArray;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const repository = repositories.find((repo) => repo.id === id);
+
+  if (!repository) {
+    return response.status(400).json("Repository not found");
+  }
+
+  repositories = repositories.filter((repo) => repo.id !== id);
+
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const repository = repositories.find((repo) => repo.id === id);
+
+  if (!repository) {
+    return response.status(400).json("Repository not found");
+  }
+
+  Object.assign(repository, {
+    likes: repository.likes + 1,
+  });
+
+  const newArray = repositories.filter((repo) => repo.id !== id);
+  newArray.push(repository);
+
+  repositories = newArray;
+
+  return response.json(repository);
 });
 
 module.exports = app;
